b5a07bc801628f6e1e3ea0a1cc89a84b939ad1b4
Stuart Axon <stuaxo2@yahoo.com>
2015-11-06 22:21:22 +0800

    Initial add

---
+++ examples/highscores.py
@@ -0,0 +1,34 @@
+## example - create a highscore table indexable by score, name and species
+
+from multidex import MultiIndex
+
+from operator import attrgetter
+from collections import namedtuple
+
+HighScore = namedtuple("HighScore", "name score species")
+
+class HighScores(object):
+    __metaclass__ = MultiIndex
+
+    # specify index name and getter
+    alt_indexes = dict(
+        species=attrgetter("species"),
+        score=attrgetter("score")
+    )
+        
+score_idx = HighScores()
+
+highscores = [
+    HighScore("terry", 200, "cat"),
+    HighScore("jill", 1900, "human"),
+    HighScore("zoooby", 100, "cat"),
+    HighScore("chilax", 100, "human"),
+]
+
+for score in highscores:
+    score_idx[score.name] = score
+
+for species in ["cat", "human"]:
+    print("Top %s scores" % species)
+    for highscore in sorted(score_idx.find("species", species), key=attrgetter("score")):
+        print("*** %s *** %s" % (highscore.name, highscore.score))
---
+++ multidex/__init__.py
@@ -0,0 +1,67 @@
+import operator
+
+
+class MultiIndexBase(dict):
+    def __delitem__(self, key):
+        item = self[key]
+        super(MultiIndexIndexBase, self).__delitem__(key)
+        self.__deindex_item__(item)
+
+    def __setitem__(self, key, value):
+        super(MultiIndexBase, self).__setitem__(key, value)
+        self.__index_item__(value)
+        
+    def find(self, name, value):
+        """
+        return sequence of all matching items
+        """
+        index = getattr(self, '__%s_index__' % name)
+        return tuple(index.get(value, []))
+        
+class MultiIndex(type):
+    """
+    Metaclass enables instance methods to be used as handlers.
+    """
+    def __new__(meta, name, bases, dct):
+        # Ideally the indexes would be weakrefdicts but
+        # str cannot be used in them
+        try:
+            alt_indexes = dct.pop("alt_indexes")
+            for name, getter in alt_indexes.items():
+                dct['__%s_index__' % name] = {}
+
+            def __index_item__(self, item):
+                for name, getter in alt_indexes.items():
+                    index = dct['__%s_index__' % name]
+                    
+                    key = getter(item)
+                    items = index.get(key)
+                    if items is None:
+                        #items = weakref.WeakSet([item])
+                        items = {item}
+                        index[key] = items
+                    else:
+                        items.add(item)
+                        
+            def __deindex_item__(self, item):
+                for name, getter in alt_indexes.items():
+                    index = dct['__%s_index__' % name]
+                    
+                    key = getter(item)
+                    items = index.get(key)
+                    if items is not None:
+                        items.remove(item)
+                        if not items:
+                            del index[key]
+
+            
+            dct['__deindex_item__'] = __deindex_item__
+            dct['__index_item__'] = __index_item__
+        except IndexError:
+            raise ValueError("Missing alt_indexes")
+        
+        bases = (MultiIndexBase,) + bases
+        return super(MultiIndex, meta).__new__(meta, name, bases, dct)
+    
+    def __init__(cls, name, bases, dct):
+        super(MultiIndex, cls).__init__(name, bases, dct)
